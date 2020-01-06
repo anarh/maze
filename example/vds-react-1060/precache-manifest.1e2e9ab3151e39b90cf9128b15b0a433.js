ecb762ae637206cde2d5f4f1933495cfe719c936
Emmanuel Addo Narh <enarh@visa.com>
2020-01-06 21:25:40 +0000

    added 1060

---
+++ example/vds-react-1060/precache-manifest.1e2e9ab3151e39b90cf9128b15b0a433.js
@@ -0,0 +1,374 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c1d3eb32705df177eb7ebdc20a52f6",
+    "url": "/projects/vds-react-1060/index.html"
+  },
+  {
+    "revision": "39f5526ce76faf561f58",
+    "url": "/projects/vds-react-1060/static/css/16.8ae077f7.chunk.css"
+  },
+  {
+    "revision": "0a491369cca18eb6499b",
+    "url": "/projects/vds-react-1060/static/css/66.8c1731e0.chunk.css"
+  },
+  {
+    "revision": "dfb991f770929c150a54",
+    "url": "/projects/vds-react-1060/static/css/main.0aadc8f4.chunk.css"
+  },
+  {
+    "revision": "98c4f701d62b24084190",
+    "url": "/projects/vds-react-1060/static/js/0.23fdbc19.chunk.js"
+  },
+  {
+    "revision": "6cabd8f47413b920aac2",
+    "url": "/projects/vds-react-1060/static/js/1.08ed3590.chunk.js"
+  },
+  {
+    "revision": "35dbeec613df7be4ebaf",
+    "url": "/projects/vds-react-1060/static/js/10.7b10ef0b.chunk.js"
+  },
+  {
+    "revision": "bad440d0c5a98b323e7a",
+    "url": "/projects/vds-react-1060/static/js/11.d9f769d1.chunk.js"
+  },
+  {
+    "revision": "3ef7a87a0cefd5665712",
+    "url": "/projects/vds-react-1060/static/js/12.c14094af.chunk.js"
+  },
+  {
+    "revision": "1185de06502f9cd3c217",
+    "url": "/projects/vds-react-1060/static/js/13.6a618afe.chunk.js"
+  },
+  {
+    "revision": "3a38a98d60e5c77b1136",
+    "url": "/projects/vds-react-1060/static/js/14.2606cbb8.chunk.js"
+  },
+  {
+    "revision": "0b562b0e0d43526cf5ff",
+    "url": "/projects/vds-react-1060/static/js/15.b5e93897.chunk.js"
+  },
+  {
+    "revision": "39f5526ce76faf561f58",
+    "url": "/projects/vds-react-1060/static/js/16.e1fa6a32.chunk.js"
+  },
+  {
+    "revision": "c4f405c9f9e26be626f9",
+    "url": "/projects/vds-react-1060/static/js/17.d5bd1465.chunk.js"
+  },
+  {
+    "revision": "1ad6b2a658760a290f37",
+    "url": "/projects/vds-react-1060/static/js/18.dcb067fa.chunk.js"
+  },
+  {
+    "revision": "028437ea18ef7f1dad0e",
+    "url": "/projects/vds-react-1060/static/js/19.0cf3d448.chunk.js"
+  },
+  {
+    "revision": "732321720696eb66e334",
+    "url": "/projects/vds-react-1060/static/js/2.a6874285.chunk.js"
+  },
+  {
+    "revision": "4a57d92ca1887a727847",
+    "url": "/projects/vds-react-1060/static/js/20.bdae0452.chunk.js"
+  },
+  {
+    "revision": "c9774e9abfa46da58d54",
+    "url": "/projects/vds-react-1060/static/js/21.c757cd82.chunk.js"
+  },
+  {
+    "revision": "ae2398dc8384938a5600",
+    "url": "/projects/vds-react-1060/static/js/22.9729b8a2.chunk.js"
+  },
+  {
+    "revision": "ec76d3acd425a1bb7e17",
+    "url": "/projects/vds-react-1060/static/js/23.8d188b0d.chunk.js"
+  },
+  {
+    "revision": "7c1fd54c4dd7aadb71a9",
+    "url": "/projects/vds-react-1060/static/js/24.c621db80.chunk.js"
+  },
+  {
+    "revision": "024d6e43eb14006a8aa6",
+    "url": "/projects/vds-react-1060/static/js/25.489801b0.chunk.js"
+  },
+  {
+    "revision": "c4f810f790069b9ef938",
+    "url": "/projects/vds-react-1060/static/js/26.c1eb41ca.chunk.js"
+  },
+  {
+    "revision": "d662b29a879a3c928f26",
+    "url": "/projects/vds-react-1060/static/js/27.8a5b8624.chunk.js"
+  },
+  {
+    "revision": "3aef500d844556d42254",
+    "url": "/projects/vds-react-1060/static/js/28.b5fb1e12.chunk.js"
+  },
+  {
+    "revision": "eb5e1ccac48e8efeed0d",
+    "url": "/projects/vds-react-1060/static/js/29.eb8c526a.chunk.js"
+  },
+  {
+    "revision": "2583daa1a99f1381f7d1",
+    "url": "/projects/vds-react-1060/static/js/3.230fc2fe.chunk.js"
+  },
+  {
+    "revision": "493cce4123170d458ca7",
+    "url": "/projects/vds-react-1060/static/js/30.9f731cd7.chunk.js"
+  },
+  {
+    "revision": "166838615d7b42723841",
+    "url": "/projects/vds-react-1060/static/js/31.e20850df.chunk.js"
+  },
+  {
+    "revision": "a9e69eba7d22741c094f",
+    "url": "/projects/vds-react-1060/static/js/32.1d82fffe.chunk.js"
+  },
+  {
+    "revision": "20b983e904e716f118eb",
+    "url": "/projects/vds-react-1060/static/js/33.24415013.chunk.js"
+  },
+  {
+    "revision": "8bc5499cc0dd3ee36a2e",
+    "url": "/projects/vds-react-1060/static/js/34.63d06619.chunk.js"
+  },
+  {
+    "revision": "43017dc535b8119f4450",
+    "url": "/projects/vds-react-1060/static/js/35.504534a3.chunk.js"
+  },
+  {
+    "revision": "05e9f8ed63b9ccd80402",
+    "url": "/projects/vds-react-1060/static/js/36.b3e2c023.chunk.js"
+  },
+  {
+    "revision": "d0b15e09979773f75f2f",
+    "url": "/projects/vds-react-1060/static/js/37.b61a4a8a.chunk.js"
+  },
+  {
+    "revision": "7dfc551ecf309c8c5c7f",
+    "url": "/projects/vds-react-1060/static/js/38.e24aab41.chunk.js"
+  },
+  {
+    "revision": "f7727c4e959d9f73392b",
+    "url": "/projects/vds-react-1060/static/js/39.2b246310.chunk.js"
+  },
+  {
+    "revision": "0f320cf308e27324e0b3",
+    "url": "/projects/vds-react-1060/static/js/4.ebb35ad4.chunk.js"
+  },
+  {
+    "revision": "44ef05aaf6095a88998f",
+    "url": "/projects/vds-react-1060/static/js/40.a18a05a1.chunk.js"
+  },
+  {
+    "revision": "719abeab0f6b0ac30969",
+    "url": "/projects/vds-react-1060/static/js/41.bf5904ec.chunk.js"
+  },
+  {
+    "revision": "093a2d2e84201ac5410a",
+    "url": "/projects/vds-react-1060/static/js/42.4f586e25.chunk.js"
+  },
+  {
+    "revision": "bab761d9445f1d4b55a9",
+    "url": "/projects/vds-react-1060/static/js/43.3aeee1d9.chunk.js"
+  },
+  {
+    "revision": "4c3f36f7cb990aa32b27",
+    "url": "/projects/vds-react-1060/static/js/44.17671eb4.chunk.js"
+  },
+  {
+    "revision": "9c5c675af34b3de078c0",
+    "url": "/projects/vds-react-1060/static/js/45.bbeeac69.chunk.js"
+  },
+  {
+    "revision": "d4525d10188c7ed661a3",
+    "url": "/projects/vds-react-1060/static/js/46.83ce6796.chunk.js"
+  },
+  {
+    "revision": "0a0f6ae6dc58001d2967",
+    "url": "/projects/vds-react-1060/static/js/47.88c051be.chunk.js"
+  },
+  {
+    "revision": "7b8b26a23b78973480e6",
+    "url": "/projects/vds-react-1060/static/js/48.367c8be2.chunk.js"
+  },
+  {
+    "revision": "89c759bd1da665371692",
+    "url": "/projects/vds-react-1060/static/js/49.385a09a7.chunk.js"
+  },
+  {
+    "revision": "fcfda018c981cb825680",
+    "url": "/projects/vds-react-1060/static/js/5.4b93d942.chunk.js"
+  },
+  {
+    "revision": "7a50d073cb2563a8d2d8",
+    "url": "/projects/vds-react-1060/static/js/50.c16c9155.chunk.js"
+  },
+  {
+    "revision": "7ad5ea8ceac4bf677ae5",
+    "url": "/projects/vds-react-1060/static/js/51.9415d418.chunk.js"
+  },
+  {
+    "revision": "02254a05580052e146f7",
+    "url": "/projects/vds-react-1060/static/js/52.a8ef79ab.chunk.js"
+  },
+  {
+    "revision": "4615f3a1dca1731ebb9f",
+    "url": "/projects/vds-react-1060/static/js/53.faaf12df.chunk.js"
+  },
+  {
+    "revision": "2b9e75654a5503d8409f",
+    "url": "/projects/vds-react-1060/static/js/54.f7cf6bf2.chunk.js"
+  },
+  {
+    "revision": "ad31353b61d6ddffe827",
+    "url": "/projects/vds-react-1060/static/js/55.c11c8ef9.chunk.js"
+  },
+  {
+    "revision": "359cba151338c310b731",
+    "url": "/projects/vds-react-1060/static/js/56.612d5eca.chunk.js"
+  },
+  {
+    "revision": "eb1c864689ff151f87fa",
+    "url": "/projects/vds-react-1060/static/js/57.80281dcc.chunk.js"
+  },
+  {
+    "revision": "0b2d81734213f2df426f",
+    "url": "/projects/vds-react-1060/static/js/58.87304162.chunk.js"
+  },
+  {
+    "revision": "455eec50940299172d46",
+    "url": "/projects/vds-react-1060/static/js/59.11f06dda.chunk.js"
+  },
+  {
+    "revision": "d4d89ee765f332f4f230",
+    "url": "/projects/vds-react-1060/static/js/6.dd2a48e5.chunk.js"
+  },
+  {
+    "revision": "750d15d1920bade998e8",
+    "url": "/projects/vds-react-1060/static/js/60.f7746d50.chunk.js"
+  },
+  {
+    "revision": "afbd1aa8cf37176026c3",
+    "url": "/projects/vds-react-1060/static/js/61.e938f335.chunk.js"
+  },
+  {
+    "revision": "f0503b1c7698ba3478d9",
+    "url": "/projects/vds-react-1060/static/js/62.dea4a8e1.chunk.js"
+  },
+  {
+    "revision": "5d9a5563f4322267a6ca",
+    "url": "/projects/vds-react-1060/static/js/63.522ffe0d.chunk.js"
+  },
+  {
+    "revision": "a27d95fdbf10ce09b309",
+    "url": "/projects/vds-react-1060/static/js/64.caef7341.chunk.js"
+  },
+  {
+    "revision": "844207fb1b196b875b71",
+    "url": "/projects/vds-react-1060/static/js/65.76bd0aa9.chunk.js"
+  },
+  {
+    "revision": "0a491369cca18eb6499b",
+    "url": "/projects/vds-react-1060/static/js/66.73fd813b.chunk.js"
+  },
+  {
+    "revision": "218745a5d84cae94c500",
+    "url": "/projects/vds-react-1060/static/js/67.4aa53cef.chunk.js"
+  },
+  {
+    "revision": "7ff70f1b5a849343635a",
+    "url": "/projects/vds-react-1060/static/js/68.b8969889.chunk.js"
+  },
+  {
+    "revision": "2b1c327ec4adb8cdcf0a",
+    "url": "/projects/vds-react-1060/static/js/69.cce40514.chunk.js"
+  },
+  {
+    "revision": "fb8fad79784ccf2615db",
+    "url": "/projects/vds-react-1060/static/js/7.21c93e68.chunk.js"
+  },
+  {
+    "revision": "bf66a6e405f26e8d48e2",
+    "url": "/projects/vds-react-1060/static/js/70.180df244.chunk.js"
+  },
+  {
+    "revision": "1604987a7ef8d8ec427e",
+    "url": "/projects/vds-react-1060/static/js/73.2da2af74.chunk.js"
+  },
+  {
+    "revision": "ad76401faf9977b303af",
+    "url": "/projects/vds-react-1060/static/js/8.b3221def.chunk.js"
+  },
+  {
+    "revision": "6bc18917a1d35dcb2cbc",
+    "url": "/projects/vds-react-1060/static/js/9.60491a5c.chunk.js"
+  },
+  {
+    "revision": "dfb991f770929c150a54",
+    "url": "/projects/vds-react-1060/static/js/main.fa3002a1.chunk.js"
+  },
+  {
+    "revision": "5bdea369c8c5de580f6a",
+    "url": "/projects/vds-react-1060/static/js/runtime-main.6b57bb76.js"
+  },
+  {
+    "revision": "09e00aa7622ece30a0f1e06b55f66c2a",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Light-webfont.09e00aa7.eot"
+  },
+  {
+    "revision": "2e98fc3ce85f31f63010b706259cb604",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Light-webfont.2e98fc3c.ttf"
+  },
+  {
+    "revision": "8f04ed9aeb2185499068d84842b95aa1",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Light-webfont.8f04ed9a.svg"
+  },
+  {
+    "revision": "488d5cc145299ba07b75495100419ee6",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Regular-webfont.488d5cc1.ttf"
+  },
+  {
+    "revision": "8185eb3059c46e4169ce107dfcf85950",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Regular-webfont.8185eb30.svg"
+  },
+  {
+    "revision": "c4d82460ef260eb1589e73528cbfb257",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Regular-webfont.c4d82460.eot"
+  },
+  {
+    "revision": "3f6b1eed8a0832d6f316fc26526348a8",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Semibold-webfont.3f6b1eed.svg"
+  },
+  {
+    "revision": "b32acea6fd3c228b5059042c7ad21c55",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Semibold-webfont.b32acea6.ttf"
+  },
+  {
+    "revision": "f28eb362fb6afe946d822ee5451c2146",
+    "url": "/projects/vds-react-1060/static/media/OpenSans-Semibold-webfont.f28eb362.eot"
+  },
+  {
+    "revision": "cdfdee212f057c02acf92dedbe6b6920",
+    "url": "/projects/vds-react-1060/static/media/fpo-card-category-graphic-02.cdfdee21.png"
+  },
+  {
+    "revision": "56f28ee9fa69a642b2eac8a597bcff1e",
+    "url": "/projects/vds-react-1060/static/media/fpo-card-category-graphic-03.56f28ee9.png"
+  },
+  {
+    "revision": "84056c725dfa5ab74f6ea232a88ed226",
+    "url": "/projects/vds-react-1060/static/media/fpo-card-data-visualization.84056c72.png"
+  },
+  {
+    "revision": "6026f3bd315e08b570090bc5c0f0e223",
+    "url": "/projects/vds-react-1060/static/media/vds_issuer_fdnb-wht.6026f3bd.svg"
+  },
+  {
+    "revision": "709e2e612f005e5620e7fd5cfd4e0a60",
+    "url": "/projects/vds-react-1060/static/media/visa_vbm_blu.709e2e61.svg"
+  },
+  {
+    "revision": "954d10317f859d90217357b39febc932",
+    "url": "/projects/vds-react-1060/static/media/visa_vbm_wht.954d1031.svg"
+  }
+]);
